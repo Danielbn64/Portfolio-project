5993947a779d05e58461202b1a43254bc2c708a4
Daniel <dnlbn164@gmailo.com>
2022-12-02 13:46:40 +0100

    first commit

---
+++ Ejemplo-Rutas.js
@@ -0,0 +1,11 @@
+//Ejemplo Rutas:
+// app.post('/test',(req, res) => {
+    //     console.log(req.body.nombre);//Dato que se envia desde el body
+    //     console.log(req.query.web);//Dato que se encuentra en la url de la peticion
+    //     console.log(req.params.id);//Dato que se encuentra como parametro en la ruta del archivo ejemplo:
+    //                                                                                         //app.post('/test/id'
+    //     res.status(200).send({
+        //         message:"Hola mundo desde mi API de NodeJS"
+        //     });
+        // });
+        
---
+++ backend/app.js
@@ -0,0 +1,28 @@
+'use strict'
+//Crear un servidor:
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express()
+
+//Archivos de rutas:
+const projectRoutes = require('./routes/project');
+
+//Middlewares:
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Configurar cabeceras y cors:
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+    });
+
+//Rutas:
+app.use('/api', projectRoutes);
+
+
+//Exportar:
+module.exports = app;
